c43c089e7aa10887367dc7dca2cdb12ec217ce6d
Shipow <shipowlata@gmail.com>
2016-01-14 10:22:56 +0100

    Added Gulp + svg symbols

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var svgSymbols = require('gulp-svg-symbols');
+var imagemin = require('gulp-imagemin');
+var inject = require('gulp-inject');
+// var pngquant = require('imagemin-pngquant'); // $ npm i -D imagemin-pngquant
+
+gulp.task('imagemin', function () {
+  return gulp.src('src/images/*')
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{removeViewBox: false}]
+      // use: [pngquant()]
+    }))
+    .pipe(gulp.dest('dist/images'));
+});
+
+//.pipe(inject(svgs, { transform: fileContents }))
+gulp.task('inject', function () {
+  var target = gulp.src('index.html');
+  // It's not necessary to read the files (will speed up things), we're only after their paths:
+  var sources = gulp.src(['assets/svg-symbols.svg'], {read: false,transform: fileContents});
+
+  return target.pipe(inject(sources))
+    .pipe(gulp.dest('/'));
+});
+
+gulp.task('sprites', function () {
+  return gulp.src('svg/*.svg')
+    .pipe(svgSymbols())
+    .pipe(gulp.dest('assets'));
+});
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
