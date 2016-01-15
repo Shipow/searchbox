24d16bd6a6bdc8ea2397ca7ed2c255ea57e25546
Shipow <shipowlata@gmail.com>
2016-01-16 00:54:51 +0100

    Custom them

--- build/js/app.js
+++ build/js/app.js
@@ -1,8 +1,6 @@
-
 $('.q').on('input', function() {
     $(this).closest('.awesome-searchbox').find('.clear-q').removeClass('hide');
 });
-
 $('.clear-q').on('click', function() {
   $(this).closest('.awesome-searchbox').find('.q').val('').focus();
   $(this).addClass('hide');
@@ -12,11 +10,31 @@ var Sass = new Sass();
 
 $('.snippet code.language-markup').text($('.awesome-searchbox').parent().html());
 
-$.get('_awesome-searchbox.scss', function(data){
-  Sass.compile(data, function(result) {
-    $('.snippet code.language-css').text(result.text);
-    Prism.highlightAll(false);
-  });
-});
+function updateSnippet(){
+  $.get('_awesome-searchbox.scss', function(data){
 
-// var scss = '$someVar: 123px; .some-selector { width: $someVar; }';
+    var config = {};
+    $('form#settings').serializeArray().map(function(item) {
+        if ( config[item.name] ) {
+            if ( typeof(config[item.name]) === "string" ) {
+                config[item.name] = [config[item.name]];
+            }
+            config[item.name].push(item.value);
+        } else {
+            config[item.name] = item.value;
+        }
+    });
+
+    config = JSON.stringify(config).replace(/"|{|}/g, '').replace(/,/g, ';');
+
+    Sass.writeFile('settings.scss', config + ';');
+
+
+    Sass.compile('@import "settings";' + data, function(result) {
+      $('.snippet code.language-css').text(result.text);
+      $("<style>" + result.text + "</style>").appendTo( "head" );
+      Prism.highlightAll(false);
+    });
+  });
+};
+$('.btn-update').on('click',updateSnippet);
