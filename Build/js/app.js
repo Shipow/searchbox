a38afa0a5d2dae2ca52bcb1cd5d41edca1ffdee6
Shipow <shipowlata@gmail.com>
2016-01-15 16:04:35 +0100

    delete Build

--- Build/js/app.js
+++
@@ -1,9 +0,0 @@
-
-$('.q').on('input', function() {
-    $(this).closest('.awesome-searchbox').find('.clear-q').removeClass('hide');
-});
-
-$('.clear-q').on('click', function() {
-  $(this).closest('.awesome-searchbox').find('.q').val('').focus();
-  $(this).addClass('hide');
-});
