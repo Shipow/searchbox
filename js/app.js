7a631a6850ec31c2f2ce529e07e8dd035ce9cb3c
Shipow <shipowlata@gmail.com>
2016-01-21 13:33:23 +0100

    Update 2016-01-21T12:33:21.891Z

--- js/app.js
+++ js/app.js
@@ -1,26 +1,9 @@
-// asb
-
-$('.js-search-input').on('input', function() {
-    $(this).closest('.awesome-searchbox').find('.js-search-reset').removeClass('hide');
-    if($(this).val().length === 0){
-      $(this).closest('.awesome-searchbox').find('.js-search-reset').addClass('hide')
-    }
-});
-$('.js-search-reset').on('click', function() {
-  $(this).closest('.awesome-searchbox').find('.js-search-input').val('').focus();
-  $(this).addClass('hide');
-});
-
-
-// demos
-
 var Sass = new Sass();
-
 var html, scss, css, js;
 
 function updateSnippet(){
 
-  $.get('awesome-searchbox.scss', function(data){
+  $.get('scss/awesome-searchbox.scss', function(data){
 
     var config = {};
     $('form#settings').serializeArray().map(function(item) {
