5d41304e3ffddf1f1c41c8e5b7153e5cec053e29
Shipow <shipowlata@gmail.com>
2016-01-23 08:17:17 +0100

    Restore some js

--- js/awesome-searchbox.js
+++ js/awesome-searchbox.js
@@ -1,10 +1,5 @@
-$('.js-search-input').on('input', function() {
-    $(this).closest('.awesome-searchbox').find('.js-search-reset').removeClass('hide');
-    if($(this).val().length === 0){
-      $(this).closest('.awesome-searchbox').find('.js-search-reset').addClass('hide')
-    }
-});
-$('.js-search-reset').on('click', function() {
-  $(this).closest('.awesome-searchbox').find('.js-search-input').val('').focus();
-  $(this).addClass('hide');
+
+// Focus search input after hitting reset button
+$('.awesome-searchbox [type="reset"]').on('click', function() {
+  $(this).siblings('[type="search"]').focus();
 });
